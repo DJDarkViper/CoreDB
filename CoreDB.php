998bad5e6c0b8b1094c4b61ddeb705a40256a5c4
Kyle Harrison <kyle@navigatormm.com>
2013-02-27 12:04:49 -0800

    fleshing out some more similarities with CoreData

--- CoreDB.php
+++ CoreDB.php
@@ -18,6 +18,19 @@ class CoreDB {
 
 }
 
+class CoreError {
+	private $code = 0;
+	private $message = null;
+
+	public CoreError($code, $message) {
+		$this->code = $code; 
+		$this->message = $message;
+	}
+
+	public function describe() {
+		return "Error: ".$this->code."; with Message: '".$this->message."'";
+	}
+}
 
 class CorePredicate {
 
@@ -25,17 +38,17 @@ class CorePredicate {
 	const DOES_NOT_EQUAL = "!=";
 	const LIKE 	 = "LIKE";
 	
-	private $field = null;
+	private $property = null;
 	private $conditional = null;
 	private $value = null;
 
-	function CoreCondition($field, $value, $conditional = self::EQUALS) {
-		$this->setField($field);
+	function CoreCondition($property, $value, $conditional = self::EQUALS) {
+		$this->setField($property);
 		$this->setValue($value);
 		$this->setConditional($conditional);
 	}
 
-	private function setField($str) { $this->field = $str; }
+	private function setProperty($str) { $this->property = $str; }
 	private function setValue($str) { $this->value = $str; }
 	private function setConditional($str) { $this->conditional = $str; }
 
@@ -46,18 +59,18 @@ class CoreSortDescriptor {
 	const ASCENDING = 1;
 	const DESCENDING = 2;
 
-	private $field = null;
+	private $property = null;
 	private $direction = null;
 
-	function CoreSort($field, $withDirection = self::ASCENDING) {
-		$this->setField($field);
+	function CoreSort($property, $withDirection = self::ASCENDING) {
+		$this->setProperty($property);
 		$this->setDirection($withDirection);
 	}
 
 }
 
 
-class CoreFetch {
+class CoreFetchRequest {
 
 	// the table
 	private $entity = null;
@@ -71,25 +84,55 @@ class CoreFetch {
 	// fields to fetch
 	private $properties = array();
 
+	// record limit
+	private $limit = null;
+
+	// automatic record offset with limit
+	private $page  = null;
+
+
 	function CoreFetch($withEntityName) {
 
 	}
 
+	public function setEntity($entityName) {
+		$this->entity = $entityName; 
+		return $this;
+	}
+
 	public function setSortDescriptor(CoreSortDescriptor $sort) {
 
+		return $this;
 	}
 
 	public function setPredicate(CorePredicate $predicate) {
 
+		return $this;
 	}
 
 	public function setPropertiesToFetch($array = array("*")) {
-		
+
+		return $this;
 	}
+
+	public function setFetchLimit($integer) {
+		$this->limit = $integer;
+		return $this;
+	}
+
+
 
 }
 
 
 class CoreContext {
+
+	public CoreContext($DatabasePath) {
+		
+	}
+
+	public function executeFetchRequest(CoreFetchRequest $request) {
+
+	}
 
 }
