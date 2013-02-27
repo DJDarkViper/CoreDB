a8e3730833b87c7b140d78c640fd0cfb8af0ee12
Kyle Harrison <kyle@navigatormm.com>
2013-02-27 11:30:08 -0800

    some basic starting stuff

---
+++ CoreDB.php
@@ -0,0 +1,53 @@
+<?
+/*
+
+
+
+
+
+
+
+*/
+
+
+class CoreDB {
+	
+	function CoreDB() {
+
+	}
+
+}
+
+
+class CoreCondition {
+
+	function CoreCondition() {
+
+	}
+
+}
+
+class CoreSort {
+
+	const ASCENDING = 1;
+	const DESCENDING = 2;
+
+	function CoreSort {
+
+	}
+
+}
+
+
+class CoreFetch {
+
+	function CoreFetch() {
+
+	}
+
+}
+
+
+class CoreContext {
+
+}
