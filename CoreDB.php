b6cdf9275bd5ac95ba0e9da884ed6f8019fd0cd3
Kyle Harrison <kyle@navigatormm.com>
2013-02-27 12:13:03 -0800

    fleshing out the methods again with more basic and expected capabilities

--- CoreDB.php
+++ CoreDB.php
@@ -37,20 +37,27 @@ class CorePredicate {
 	const EQUALS = "=";
 	const DOES_NOT_EQUAL = "!=";
 	const LIKE 	 = "LIKE";
+
+	const GLUE_AND = " AND ";
+	const GLUE_OR  = " OR ";
 	
 	private $property = null;
 	private $conditional = null;
 	private $value = null;
 
-	function CoreCondition($property, $value, $conditional = self::EQUALS) {
+	private $glue = null;
+
+	function CoreCondition($property, $value, $conditional = self::EQUALS, $glue = null) {
 		$this->setField($property);
 		$this->setValue($value);
 		$this->setConditional($conditional);
+		$this->setGlue($glue);
 	}
 
-	private function setProperty($str) { $this->property = $str; }
-	private function setValue($str) { $this->value = $str; }
-	private function setConditional($str) { $this->conditional = $str; }
+	public function setProperty($str) { $this->property = $str; }
+	public function setValue($str) { $this->value = $str; }
+	public function setConditional($str) { $this->conditional = $str; }
+	public function setGlue($glue) { $this->glue = $glue; }
 
 }
 
@@ -66,6 +73,9 @@ class CoreSortDescriptor {
 		$this->setProperty($property);
 		$this->setDirection($withDirection);
 	}
+
+	public function setProperty($property) { $this->property = $property; }
+	public function setDirection($withDirection) { $this->direction = $withDirection; }
 
 }
 
@@ -100,12 +110,34 @@ class CoreFetchRequest {
 		return $this;
 	}
 
+	/**
+	* Sets/Overrides existing Sorting description
+	* @property $sort CoreSortDescriptor a single sort descriptor object
+	*/
 	public function setSortDescriptor(CoreSortDescriptor $sort) {
 
 		return $this;
 	}
 
+	/**
+	* Sets/Overrides existing filter predication (conditional)
+	* @property $predicate CorePredicate a single predicate object (condition)
+	*/
 	public function setPredicate(CorePredicate $predicate) {
+
+		return $this;
+	}
+
+	/**
+	* Sets/Overrides existing Sorting descroption with a series of of new Sort descroptions
+	* @property $sortDescriptors An array of Sort Descriptors, requires a Glue
+	*/
+	public function setSortDescriptors($sortDescriptors) {
+
+		return $this;
+	}
+
+	public function setPredicates($predicates) {
 
 		return $this;
 	}
@@ -127,8 +159,11 @@ class CoreFetchRequest {
 
 class CoreContext {
 
+	/**
+	* Establishes a connection to the sqlite3 database
+	*/
 	public CoreContext($DatabasePath) {
-		
+
 	}
 
 	public function executeFetchRequest(CoreFetchRequest $request) {
